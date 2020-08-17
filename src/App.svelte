cd987d0affbfe617885a89e2d9b4374b1c6c51bd
Espen Klem <espen.klem@gmail.com>
2020-08-17 16:08:20 +0200

    Svelte + search-index working in chrome

---
+++ src/App.svelte
@@ -0,0 +1,59 @@
+<script>
+	import EventEmitter from 'events'
+	import {inherits} from 'util'
+
+	import si from 'search-index'
+	export let name
+
+	const idx =  si({ name: 'svelte-idx' })
+
+	const readJSONData = function (url) {
+		console.log('soon to read JSON data')
+		fetch('https://raw.githubusercontent.com/eklem/dataset-vinmonopolet/master/dataset-vinmonopolet-sparkling.json')
+			.then(response => response.json())	
+			.then(JSONdata => indexJSONData(JSONdata))
+			.catch(function (err) {
+				console.log('Error while reading data: \n' + err.message)
+			})
+	}
+
+	const indexJSONData = function (data) {
+		console.log('soon indexing data: ' + JSON.stringify(data))
+		idx.PUT(data)
+			.then(function (message) {
+				console.log('Data indexed. Number of documents indexed: ' + message)
+			})
+			.catch(function (err) {
+				console.log('Error while indexing: \n' + err.message)
+			})
+	}
+
+	readJSONData()
+	
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
